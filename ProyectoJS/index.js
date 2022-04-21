887504de39aeec27e10ead82b5be7dd527af128a
Lucas <Lucaspaoletti8@gmail.com>
2022-04-21 16:14:31 -0300

    Agregue index html/js

--- ProyectoJS/index.js
+++ ProyectoJS/index.js
@@ -56,13 +56,41 @@ alert("puto"); */
 // WHILE SIEMPRE HAY QUE PREGUNTAR Y GUARDAR EN VARIABLE PARA LUEGO CHEQUEAR EN EL CICLO, CUANDO SE CUMPLE
 //ADENTRO DEL CICLO, SALE DE EL.
 
-/* var producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo")
+
+/* var contraseña = document.getElementById('asd'); */
+
+
+//VARIABLES Y CONSTANTES
+const IVA = 1.21;
+let alineacion = 2000;
+let balanceo = 4000;
+let descuento = 0.1;
+var producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo")
+
 
 while(producto.toUpperCase() !== "A" && producto.toUpperCase() !== "B"){
         producto = prompt("selecione que producto desea: A- Alineacion o B - Balanceo");
 }
 
-alert(producto); */
-
-var contraseña = document.getElementById('asd');
+if (producto.toUpperCase () == "A") {
+    alert ("Elegiste Alineacion! el precio es $" + alineacion)
+    do {
+    var modoPago = prompt ("Como vas a abonar ? A- EFECTIVO (10% DE DESCUENTO) o B- TARJETA (RECARGO DEL IVA)")
+}  while (modoPago.toUpperCase() !== "A" && modoPago.toUpperCase() !== "B")  
+    if (modoPago.toUpperCase () == "A") {
+        alert ("El precio de la Alineacion con descuento en efectivo es de $" + (alineacion -(descuento*alineacion)))
+    } else {
+        alert ("El precio de la Alineacion con tarjeta mas el IVA es de $" + (alineacion*IVA))
+    }
+} else {
+    alert ("Elegiste Balanceo! el precio es $" + balanceo)
+    do {
+        var modoPago = prompt ("Como vas a abonar ? A- EFECTIVO (10% DE DESCUENTO) o B- TARJETA (RECARGO DEL IVA)")
+    }  while (modoPago.toUpperCase() !== "A" && modoPago.toUpperCase() !== "B")  
+        if (modoPago.toUpperCase () == "A") {
+            alert ("El precio de la Balanceo con descuento en efectivo es de $" + (balanceo -(descuento*balanceo)))
+        } else {
+            alert ("El precio de la Balanceo con tarjeta mas el IVA es de $" + (balanceo*IVA))
+        }
+}
 
