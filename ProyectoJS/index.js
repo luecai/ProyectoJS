ebf2c178ff6e42ac03a5c49e081685045a1bed3d
Lucas Paoletti <65571668+luecai@users.noreply.github.com>
2022-04-21 15:12:41 -0300

    Add files via upload

---
+++ ProyectoJS/index.js
@@ -0,0 +1,68 @@
+/* let producto = prompt("Seleccion que producto desea: A - Alineacion o B - Balanceo");
+
+do {
+if (producto.toUpperCase() === "A"){
+ var formaPago = prompt ("Seleccion tu forma de pago: A - En efectivo o B - Con tarjeta")
+ if (formaPago.toUpperCase() === "A") {
+     alert ("El precio de la alineacion en efectivo es de $1500")
+ } else {
+     alert ("El precio de la alineacion en tarjeta es de $4000")
+ }
+
+
+
+} else if (producto.toUpperCase() !== "") {
+    alert("ES B")
+}
+} while (producto = "A"); */
+
+/* let pass = " ";
+
+
+while (pass != "hola"){
+    pass = prompt ("Ingrese su contraseña")
+}
+alert ("Saliste del bucle pa");
+ */
+
+/*producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo")
+
+while (producto.toUpperCase() !== "A" || producto.toUpperCase()) {
+    producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo")
+}
+
+alert ("Salir");
+*/
+
+/* 
+do{
+    producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo");
+
+}while(producto.toUpperCase() !== "A" && producto.toUpperCase() !== "B" );
+
+alert ("Salir");
+ */
+
+
+/* var producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo");
+
+while(parseInt(producto) !== 1 && parseInt(producto) !== 2){
+    producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo");
+}
+
+alert("puto"); */
+
+
+// WHILE SIEMPRE HAY QUE PREGUNTAR Y GUARDAR EN VARIABLE PARA LUEGO CHEQUEAR EN EL CICLO, CUANDO SE CUMPLE
+//ADENTRO DEL CICLO, SALE DE EL.
+
+/* var producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo")
+
+while(producto.toUpperCase() !== "A" && producto.toUpperCase() !== "B"){
+        producto = prompt("selecione que producto desea: A- Alineacion o B - Balanceo");
+}
+
+alert(producto); */
+
+var contraseña = document.getElementById('asd');
+
