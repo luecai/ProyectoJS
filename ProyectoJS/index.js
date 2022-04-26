2504836c1ab2b77ae8910248f9b01a355cb91a4e
Lucas <lucaspaoletti8@gmail.com>
2022-04-25 21:50:16 -0300

    Corregi JS
    Modifique los nombres de los juegos.

--- ProyectoJS/index.js
+++ ProyectoJS/index.js
@@ -3,35 +3,35 @@
 
 //VARIABLES Y CONSTANTES
 const IVA = 1.21;
-let alineacion = 2000;
-let balanceo = 4000;
+let FIFA = 2000;
+let ASSETO = 4000;
 let descuento = 0.1;
-var producto = prompt("Seleccione que producto desea: A- Alineacion o B- Balanceo")
+var producto = prompt("Seleccione que producto desea: A- FIFA o B- ASSETO CORSA")
 
 
 while(producto.toUpperCase() !== "A" && producto.toUpperCase() !== "B"){
-        producto = prompt("selecione que producto desea: A- Alineacion o B - Balanceo");
+        producto = prompt("selecione que producto desea: A- FIFA o B - ASSETO CORSA");
 }
 
 if (producto.toUpperCase () == "A") {
-    alert ("Elegiste Alineacion! el precio es $" + alineacion)
+    alert ("Elegiste FIFA! el precio es $" + FIFA)
     do {
     var modoPago = prompt ("Como vas a abonar ? A- EFECTIVO (10% DE DESCUENTO) o B- TARJETA (RECARGO DEL IVA)")
 }  while (modoPago.toUpperCase() !== "A" && modoPago.toUpperCase() !== "B")  
     if (modoPago.toUpperCase () == "A") {
-        alert ("El precio de la Alineacion con descuento en efectivo es de $" + (alineacion -(descuento*alineacion)))
+        alert ("El precio de la FIFA con descuento en efectivo es de $" + (FIFA -(descuento*FIFA)))
     } else {
-        alert ("El precio de la Alineacion con tarjeta mas el IVA es de $" + (alineacion*IVA))
+        alert ("El precio de la FIFA con tarjeta mas el IVA es de $" + (FIFA*IVA))
     }
 } else {
-    alert ("Elegiste Balanceo! el precio es $" + balanceo)
+    alert ("Elegiste ASSETO CORSA! el precio es $" + ASSETO)
     do {
         var modoPago = prompt ("Como vas a abonar ? A- EFECTIVO (10% DE DESCUENTO) o B- TARJETA (RECARGO DEL IVA)")
     }  while (modoPago.toUpperCase() !== "A" && modoPago.toUpperCase() !== "B")  
         if (modoPago.toUpperCase () == "A") {
-            alert ("El precio de la Balanceo con descuento en efectivo es de $" + (balanceo -(descuento*balanceo)))
+            alert ("El precio de la ASSETO CORSA con descuento en efectivo es de $" + (ASSETO -(descuento*ASSETO)))
         } else {
-            alert ("El precio de la Balanceo con tarjeta mas el IVA es de $" + (balanceo*IVA))
+            alert ("El precio de la ASSETO CORSA con tarjeta mas el IVA es de $" + (ASSETO*IVA))
         }
 }
 
