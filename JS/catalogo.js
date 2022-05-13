79deb9f4b3897e683c2f336c9cf0ac83b0f74a0f
Lucas <lucaspaoletti8@gmail.com>
2022-05-12 22:26:48 -0300

    Entrega Eventos

--- JS/catalogo.js
+++ JS/catalogo.js
@@ -1,179 +1,68 @@
-// Creo las Class de Juegos y Programas
+const contenedor = document.getElementById("productos");
+
+
+// Creo las Class de Juegos
 class ProductoJuegos{
-    constructor (id, nombre, precio, tipo){
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = parseFloat(precio);
-    this.tipo = tipo;
-    }
-  
-  /*   obtenerJuegos () {
-      return `Juego: ${this.nombre}, Precio:${this.precio}`
-    }
-   */
+  constructor (id, nombre, precio, tipo, stock, imagen){
+  this.id = id;
+  this.nombre = nombre;
+  this.precio = parseFloat(precio);
+  this.tipo = tipo;
+  this.stock = parseFloat(stock);
+  this.imagen = imagen;
   }
-  
-  /* class ProductoProgramas{
-    constructor (id, nombre, precio, tipo){
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = parseFloat(precio);
-    this.tipo = tipo;
-    }
-  
-    obtenerProgramas () {
-      return `Juego: ${this.nombre}, Precio:${this.precio}`
-    }
-  
-  } */
-  
-  
-  
-  // Creo las Array
-  
-  const arrayJuegos = [];
-  const contenedor = document.getElementById("productos");
-  const tablaCarrito = document.getElementById("tablaCarrito");
-  const carrito = [];
-  
-  /* const arrayProgramas = []; */
-  
-  
-  
-  // Creo las funciones para pushear 
-  
-  function nuevoProductoJuegos(id, nombre, precio, tipo) {
-    arrayJuegos.push(new ProductoJuegos(id, nombre, precio, tipo));
-  }
-  
-  /* function nuevoProductoProgramas(id, nombre, precio, tipo) {
-    arrayProgramas.push(new ProductoProgramas(id, nombre, precio, tipo));
-  }
-   */
-  
-  
-  
-  nuevoProductoJuegos(1, "CS GO ", 600, "Shooter");
-  nuevoProductoJuegos(2, "FIFA", 1500, "Deporte");
-  nuevoProductoJuegos(3, "FORMULA 1", 1200, "Carrera");
-  nuevoProductoJuegos(4, "MAFIA 2", 1200, "Mundo Abierto");
-  nuevoProductoJuegos(5, "BATTLEFIELD", 450, "Shooter");
-  nuevoProductoJuegos(6, "CALL OF DUTY", 900, "Shooter");
-  nuevoProductoJuegos(7, "NEED FOR SPEED", 2100, "Carrera");
-  nuevoProductoJuegos(8, "FORZA", 1200, "Deporte");
-  nuevoProductoJuegos(9, "NBA", 2800, "Deporte");
-  nuevoProductoJuegos(10, "GTA", 2500, "Mundo Abierto");
-  
-  /* nuevoProductoProgramas(1, "PHOTOSHOP ", 600, "Shooter");
-  nuevoProductoProgramas(2, "COREL", 1500, "Deporte");
-  nuevoProductoProgramas(3, "AUTOCAD", 1200, "Carrera");
-  nuevoProductoProgramas(4, "SONY VEGAS", 3800, "Mundo Abierto");
-   */
-  
-  
-  
-  // Creo los for para recorrer las array
-  
-  const listadoJuegos = () => {
-    for (let i = 0; i < arrayJuegos.length; i++) {
-      console.log(arrayJuegos[i].obtenerJuegos());
-      }
-    }
-  
-  const listadoProgramas = () => {
-    for (let i = 0; i < arrayProgramas.length; i++) {
-      console.log(arrayProgramas[i].obtenerProgramas());
-      }
-    }
-  
-  
-  // Alert y Console.Log
-    
-  /* alert("Bienvenidos a SteamYA!");
-   */
-  
-  
-  /* let busqueda = prompt ("Que estas buscando juegos o programas ?");
-  
-  while(busqueda.toUpperCase() !== "JUEGOS" && busqueda.toUpperCase() !== "PROGRAMAS"){
-    busqueda = prompt("Por favor ingrese juegos o programas");
-  }
-    if(busqueda.toUpperCase() === "JUEGOS") {
-      alert("Los Juegos con los precios se veran en console!");
-      listadoJuegos();
-  
-    } else if (busqueda.toUpperCase() === "PROGRAMAS") {
-      alert("Los Programas con los precios se veran en console!");
-      listadoProgramas();
-    }else {
-      alert ("Error no encontramos nada :(")
-    }; */
-  
-  
-  const getCarta = (item) => {
-  
-  return (
-      `
-      <div><h3>${item.name}</h3></div>
-      <br>
-      <h5> Precio: ${item.precio}
-      <div> <button onclick=agregarCarrito(${item.id}) ${!item.stock ? 'disabled' : '' } >Agregar al carrito </button> </div>
-      `
-  )
-  }
-  
-  const getRow = (item) => {
-  
-  return (
-      `
-      <th scope="row">${item.id}</th>
-      <td>${item.nombre}</td>
-      <td>${item.cantidad}</td>
-      <td>$${item.precio * item.cantidad} ($${item.precio})</td>
-      `
-  )
-  }
-  
-  // función flecha
-/*   const cargarProductos = (datos, nodo, esTabla) => {
-      let acumulador = "";
-      datos.forEach((el) => {
-          acumulador += esTabla ? getRow(el) : getCarta(el);
-      })
-      nodo.innerHTML = acumulador;
-  };
-   */
 
-  const cargarProductos = (datos, nodo) => {
-    let acumulador = "";
-    datos.forEach((el) => {
-        acumulador += getCarta(el);
-    })
-    nodo.innerHTML = acumulador;
-    };
+}
 
 
 
-  const agregarCarrito = (id) => {
-      const seleccion = arrayJuegos.find(item => item.id === id);
-      const busqueda = carrito.findIndex(el => el.id === id);
-      
-      if (busqueda === -1) {
-          carrito.push({
-              id: seleccion.id,
-              nombre: seleccion.nombre,
-              precio: seleccion.precio,
-              cantidad: 1,
-              imagen: seleccion.imagen,
-          })
-      } else {
-          carrito[busqueda].cantidad = carrito[busqueda].cantidad + 1
-      }
-      
-      cargarProductos(carrito, tablaCarrito, true);
-  }
-  
-  cargarProductos(arrayJuegos, contenedor, false);
-  tablaCarrito
-  
-  console.log(arrayJuegos);
+
+// Creo las Array
+
+const PRODUCTOS = [];
+
+
+// Creo las funciones para pushear 
+
+function nuevoProductoJuegos(id, nombre, precio, tipo, stock, imagen) {
+  PRODUCTOS.push(new ProductoJuegos(id, nombre, precio, tipo, stock, imagen));
+}
+
+nuevoProductoJuegos(1, "CS GO ", 600, "Shooter", 10, "https://estnn.com/wp-content/uploads/2018/08/Counter-Strike-GO-1200x720.png");
+nuevoProductoJuegos(2, "FIFA", 1500, "Deporte", 5, "https://image.api.playstation.com/vulcan/img/rnd/202111/0822/zDXM9K2cQiq0vKTDwF0TkAor.png");
+nuevoProductoJuegos(3, "F1 22", 1200, "Carrera", 6, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIp2Pt-9FzA2BxyF9eTeAD66B0uLb4puBV5AP8In5wensGjIwMJOUJ8cTFMeV4ufn8H4I&usqp=CAU");
+nuevoProductoJuegos(4, "GTA V", 1200, "Mundo Abierto", 3, "https://static.wikia.nocookie.net/esgta/images/c/c1/GTA_V_logo.png/revision/latest?cb=20130906185925");
+
+
+
+
+const getCarta = (item) => {
+  return (`
+  <div class="card" style="width: 18rem;">
+    <img src="${item.imagen}" class="card-img-top" alt=" ${item.nombre}">
+    <div class="card-body">
+      <h5 class="card-title">${item.nombre}</h5>
+      <p class="card-text">Precio: ${item.precio}</p>
+      <p class="card-text">Stock: ${item.stock}</p>
+      <a href="#" onclick=agregarCarrito(${item.id}) class="btn btn-primary">AGREGAR AL CARRITO </a>
+    </div>
+  </div>
+  `)
+};
+
+
+const cargarProductos = (datos, nodo) => {
+  let acumulador = "";
+  datos.forEach((el) => {
+    acumulador += getCarta(el);
+  })
+  nodo.innerHTML = acumulador;
+};
+
+const agregarCarrito = (id) => {
+  const seleccion = PRODUCTOS.find(item => item.id === id);
+  alert("Agregaste " + seleccion.nombre)
+
+}
+
+cargarProductos (PRODUCTOS, contenedor);
